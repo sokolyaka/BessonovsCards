37b8096ce0af276c780d41f769ecce5e89b9757a
sokolyaka <sokolov.ob@gmail.com>
2017-10-21 23:09:13 +0300

    added NotFoundException

--- app/src/main/java/com/sokolov/bessonovscards/data/reposiroty/ICardRepository.java
+++ app/src/main/java/com/sokolov/bessonovscards/data/reposiroty/ICardRepository.java
@@ -1,7 +1,8 @@
 package com.sokolov.bessonovscards.data.reposiroty;
 
+import com.sokolov.bessonovscards.data.exceptions.NotFoundException;
 import com.sokolov.bessonovscards.entity.ICard;
 
 public interface ICardRepository {
-    void save(ICard card);
+    void save(ICard card) throws NotFoundException;
 }
