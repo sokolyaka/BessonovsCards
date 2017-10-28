387068e6fd39a122e09e7b833e4c176e7ac684bd
sokolyaka <sokolov.ob@gmail.com>
2017-10-28 15:08:17 +0300

    initialized home activity and home view, added list of categories

---
+++ app/src/main/java/com/sokolov/bessonovscards/view/home/adapter/CategoryAdapter.java
@@ -0,0 +1,44 @@
+package com.sokolov.bessonovscards.view.home.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.sokolov.bessonovscards.R;
+import com.sokolov.bessonovscards.view.home.model.ICategoryDisplayModel;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter {
+
+    private final List<ICategoryDisplayModel> categories;
+
+    public CategoryAdapter(List<ICategoryDisplayModel> categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return
+                new ViewHolder(
+                        LayoutInflater.from(parent.getContext())
+                                .inflate(R.layout.home_category, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((ViewHolder) holder).bind(categories.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public void setCategories(List<ICategoryDisplayModel> categories) {
+        this.categories.clear();
+        this.categories.addAll(categories);
+        notifyDataSetChanged();
+    }
+
+}
